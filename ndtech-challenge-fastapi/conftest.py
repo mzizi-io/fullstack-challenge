804b1ad4ac6500ccb74e02bac6610bbd94bf4071
calebmigosi <calebmigosi@gmail.com>
2025-01-24 01:14:21 +0100

    Add tests

---
+++ ndtech-challenge-fastapi/conftest.py
@@ -0,0 +1,32 @@
+import os
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+load_dotenv()
+
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_NAME = os.environ["DB_NAME"]
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@localhost:{DB_PORT}/{DB_NAME}" # noqa
+
+
+engine = create_async_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    echo=True
+)
+
+async_session_factory = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+
+@pytest_asyncio.fixture(scope="session")
+async def session():
+    yield async_session_factory()
--- ndtech-challenge-fastapi/tests/integration_tests/test_search.py
+++ ndtech-challenge-fastapi/tests/integration_tests/test_search.py
@@ -1,2 +1,23 @@
-def test_get():
-    assert True
+from src.core.search import Search
+import pytest
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_get(session):
+    res = await Search().get(session=session, page=1, per_page=10)
+    res_json = res.model_dump()
+    assert res_json["assets"][0]["pin"] == 17083150310000
+    assert res_json["assets"][1]["pin"] == 17173240300000
+    assert res_json["assets"][2]["pin"] == 17173240160000
+    assert res_json["assets"][3]["pin"] == 17173240170000
+    assert res_json["assets"][4]["pin"] == 17173240180000
+    assert res_json["assets"][5]["pin"] == 17173160420000
+    assert res_json["assets"][6]["pin"] == 17171230450000
+    assert res_json["assets"][7]["pin"] == 17173240190000
+    assert res_json["assets"][8]["pin"] == 17173160430000
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_distinct_values(session):
+    res = await Search().get_distinct_values(session=session, field="rec_type")
+    assert len(res) == 1
